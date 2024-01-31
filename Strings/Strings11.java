ed91f6c1a47191877d5050d88d75c1d57e34f6bd
Khushi Jain <khushij1011@gmail.com>
2024-01-31 21:31:17 +0530

    reverse each word of string

---
+++ Strings/Strings11.java
@@ -0,0 +1,25 @@
+//reverse each word of the string:
+
+import java.util.Scanner;
+public class Strings11 {
+    public static void main(String[] args){
+        String str = new String();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter string: ");
+        str = sc.nextLine();
+        int i, j, k; 
+        i=str.length()-1;
+        while(i>=0){
+            j = i;
+            while(str.charAt(j)!=' ' && j>0)
+                j--;
+            if(j==0)
+            k=0;
+            else{
+                k = j+1;
+            }
+            System.out.print(str.substring(k, i+1)+" ");
+            i = j-1;
+        }
+    }
+}
